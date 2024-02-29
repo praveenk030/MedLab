ab96872ab191533f38c2e43ca0d1f01f2df2178f
praveen <praveenk31@gmail.com>
2024-02-29 19:00:02 +0530

    first commit

---
+++ src/main/java/com/example/medilab/MediLabApplication.java
@@ -0,0 +1,13 @@
+package com.example.medilab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MediLabApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MediLabApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/medilab/MediLabApplicationTests.java
@@ -0,0 +1,14 @@
+package com.example.medilab;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+//@SpringBootTest
+//class MediLabApplicationTests {
+//
+//	@Test
+//	void contextLoads() {
+//
+//	}
+//
+//}
